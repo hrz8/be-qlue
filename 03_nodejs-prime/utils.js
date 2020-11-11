ebb060bf45ed4efb5eb0b0194042b347cd1b8e00
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2020-11-11 23:27:40 +0700

    clean up code

--- 03_nodejs-prime/utils.js
+++ 03_nodejs-prime/utils.js
@@ -2,7 +2,7 @@ module.exports = {
     isPrime(x) {
         if (x === 2) return true;
         if (!Number.isInteger(x) || x < 2 || x % 2 === 0) return false;
-        for (let i = 3; i <= Math.sqrt(x); i++) {
+        for (let i = 3; i <= Math.sqrt(x); i+=2) {
             if (x % i === 0) return false;
         }
         return true;
