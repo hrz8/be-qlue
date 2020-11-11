aa7a2a668e78c8734436c8ac05aeba30e692d452
Hirzi Nurfakhrian <hirzinurfakhrian@gmail.com>
2020-11-11 16:55:05 +0700

    add sql solution

--- 03_nodejs-prime/utils.js
+++ 03_nodejs-prime/utils.js
@@ -3,9 +3,7 @@ module.exports = {
         if (x === 2) return true;
         if (!Number.isInteger(x) || x < 2 || x % 2 === 0) return false;
         for (let i = 3; i <= Math.sqrt(x); i++) {
-            if (x % i === 0) {
-                return false;
-            }
+            if (x % i === 0) return false;
         }
         return true;
     }
